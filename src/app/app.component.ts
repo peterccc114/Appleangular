7a6c190e6f73da862153b7f40d8980d9234239ea
peterccc114 <prettyboy114@mail.com>
2017-07-27 05:52:26 -0700

    chore CURD and Error Handing
    CURD Operations using HTTP and Error Handig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,39 @@
 import { Component } from '@angular/core';
+import { IProduct } from './products';
+import { ProductService } from './products.service';
+//import { appService } from './app.service';
+import { Http , Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'my-app',
-  templateUrl: `app/app.component.html`,
+  template: '<div>Hello</div>',
+  providers: [ProductService]
+ // templateUrl: `app/app.component.html`,
 })
 export class AppComponent  
 {
-   name = 'Angular'; 
-   appTitle:string   = 'welcome';
-   appStatus:boolean = true;
-   appList: any[] = [{
-     "ID": "1",
-     "Name": "one"
-    },
-    {
-      "ID":"2",
-      "Name": "Two"
-    }];
+  //  name = 'Angular'; 
+  //  appTitle:string   = 'welcome';
+  //  appStatus:boolean = true;
+  //  appList: any[] = [{
+  //    "ID": "1",
+  //    "Name": "one",
+  //    "url": 'app/Images/images_download.jpg'
+  //   },
+  //   {
+  //     "ID":"2",
+  //     "Name": "Two",
+  //     "url": 'app/Images/images_download.jpg'
+  //   }];
+   iproducts: IProduct[];
+   constructor(private _product: ProductService) {
+   }
+   
+   ngOnInit() : void {
+      this._product.getproducts()
+      .subscribe(iproducts => this.iproducts = iproducts);
+   }
+  //MetaData\
 }
